1dea85837d6aa4725a4469816b8e3d8342e50ce1
nickdenisov777 <nick_denisov_777@mail.ru>
2020-07-03 00:16:08 +0300

    Task2 working on classifier models

--- Svyaznoy/Task2/task2.py
+++ Svyaznoy/Task2/task2.py
@@ -1,6 +1,16 @@
 import numpy as np
 import pandas as pd
 
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+from pandas_profiling import ProfileReport
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.model_selection import train_test_split, GridSearchCV
+from sklearn.metrics import roc_auc_score
+from sklearn.svm import SVC
+from sklearn.neighbors import KNeighborsClassifier
+
 
 def GetData(FileName):
     input_file = open(FileName, 'r')
@@ -14,7 +24,51 @@ def GetData(FileName):
     dframe.to_csv("task2.csv")
     return dframe
 
+def Visualisation(dframe):
+    fig = plt.figure(figsize = (21, 8), num = 'Корреляция признаков и ответа')
+    ax = []
+    names = list(dframe.columns)[ : -1]
+    for name in names:
+        ax.append(fig.add_subplot(2, 5, names.index(name) + 1))
+        sns.regplot(x = 'y', y = name, data = dframe, ax = ax[names.index(name)])
+    plt.subplots_adjust(wspace = 0.3)
+    plt.show()
+    fig.savefig("correlation.png")
+
+    fig = plt.figure(figsize = (12 ,9), num = 'Карта корреляции')
+    sns.heatmap(data = dframe.corr(), annot = True, vmin = -1, vmax = 1, center = 0)
+    plt.show()
+    fig.savefig("heatmap.png")
+
+def GetInfo(dframe):
+    print(dframe.describe())
+    profile = ProfileReport(dframe, title = "Task2 Data")
+    profile.to_file("task2.html")
+
+def KNNClassifier():
+    model = KNeighborsClassifier(njobs = -1)
+    return 0
+
 
 # M A I N
-df = GetData("task2.txt")
-#df = pd.read_csv("task2.csv")
+#df = GetData("task2.txt")
+df = pd.read_csv("task2.csv", index_col = 0)
+
+#Visualisation(df)
+
+'''
+for name in ['x_3', 'x_6', 'x_7', 'x_8', 'x_9', 'x_10']:
+    df[name] = pd.Series(np.array([1/val for val in df[name].to_numpy()]))
+    df[name] = pd.Series(MinMaxScaler(feature_range=(0, 1)).fit_transform(np.array([[1/val for val in df[name]]]).reshape(len(df[name]), 1)).reshape(1, len(df[name]))[0])
+'''
+
+#GetInfo(df)
+
+class_balance = dict.fromkeys([i for i in range(3, 30)], 0)
+for val in df['y']:
+    class_balance[val] += 1
+print("Процент каждого класса в выборке:", [100*class_balance[key]/len(df['y']) for key in class_balance])
+
+X = df[list(df.columns)[ : -1]]
+y = df[list(df.columns)[-1 : ]]
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.08, random_state = 16)
