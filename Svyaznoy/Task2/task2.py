36a2d0e95395fcdbcba57123758d5411b8c957fc
nickdenisov777 <nick_denisov_777@mail.ru>
2020-07-04 15:14:03 +0300

    Task2 tested all algorithms

--- Svyaznoy/Task2/task2.py
+++ Svyaznoy/Task2/task2.py
@@ -10,6 +10,8 @@ from sklearn.model_selection import train_test_split, GridSearchCV, ShuffleSplit
 from sklearn.metrics import roc_auc_score, precision_score, recall_score, accuracy_score, confusion_matrix
 from sklearn.svm import SVC
 from sklearn.neighbors import KNeighborsClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.neural_network import MLPClassifier
 
 
 def GetData(FileName):
@@ -74,15 +76,97 @@ def KNNClassifier(dframe):
         print("Real:", y_validate[i])
     return 0
 
+def RFClassifier(dframe):
+    dframe = dframe.drop(['x_6', 'x_7', 'x_8', 'x_9', 'x_10'], axis = 1)
+    X = dframe[list(dframe.columns)[ : -1]][ : -20].to_numpy()
+    y = dframe[list(dframe.columns)[-1 : ]][ : -20].to_numpy().reshape(len(dframe['y']) - 20, )
+    y = np.int_(y)
+    X_validate = dframe[list(dframe.columns)[ : -1]][-20 : ].to_numpy()
+    y_validate = dframe[list(dframe.columns)[-1 : ]][-20 : ].to_numpy()
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.08, random_state = 16)
+    model = RandomForestClassifier(n_jobs = -1, random_state = 0)
+    cv_gen = ShuffleSplit(n_splits = 10, test_size = 0.25, random_state = 0)
+    model_gs = GridSearchCV(model,
+                            {
+                                'n_estimators': [75, 100, 125],
+                                'criterion': ['gini', 'entropy'],
+                                'max_features': ['sqrt', 'log2']
+                            },
+                            scoring = 'accuracy',
+                            n_jobs = -1,
+                            cv = cv_gen
+                            )
+    #print(y)
+    model_gs.fit(X, y)
+    print(model_gs.best_params_)
+    print("Accuracy score", model_gs.best_score_)
+    for i in range(X_validate.shape[0]):
+        prediction = model_gs.best_estimator_.predict(X_validate[i].reshape(1, X_validate.shape[1]))
+        print("Predicted:", prediction)
+        print("Real:", y_validate[i])
+        print("")
+    return 0
+
 def SVMClassifier(dframe):
-    dframe = dframe.drop(['x_3', 'x_5','x_6', 'x_7', 'x_8', 'x_9', 'x_10'], axis = 1)
+    dframe = dframe.drop(['x_6', 'x_7', 'x_8', 'x_9', 'x_10'], axis = 1)
+    X = dframe[list(dframe.columns)[ : -1]][ : -20].to_numpy()
+    y = dframe[list(dframe.columns)[-1 : ]][ : -20].to_numpy().reshape(len(dframe['y']) - 20, )
+    y = np.int_(y)
+    X_validate = dframe[list(dframe.columns)[ : -1]][-20 : ].to_numpy()
+    y_validate = dframe[list(dframe.columns)[-1 : ]][-20 : ].to_numpy()
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.08, random_state = 16)
+    model = SVC(class_weight = 'balanced', verbose = 0)
+    cv_gen = ShuffleSplit(n_splits = 10, test_size = 0.25, random_state = 0)
+    model_gs = GridSearchCV(model,
+                            {
+                                #'kernel': ['linear', 'poly', 'rbf', 'sigmoid'],
+                                'kernel': ['rbf', 'sigmoid'],
+                                'gamma': ['scale', 'auto']
+                            },
+                            scoring = 'accuracy',
+                            n_jobs = -1,
+                            cv = cv_gen
+                            )
+    #print(y)
+    model_gs.fit(X, y)
+    print(model_gs.best_params_)
+    print("Accuracy score", model_gs.best_score_)
+    for i in range(X_validate.shape[0]):
+        prediction = model_gs.best_estimator_.predict(X_validate[i].reshape(1, X_validate.shape[1]))
+        print("Predicted:", prediction)
+        print("Real:", y_validate[i])
+    return 0
+
+def NeuralClassifier(dframe):
+    #print(dframe.columns)
+    dframe = dframe.drop(['x_7', 'x_9'], axis = 1)
     X = dframe[list(dframe.columns)[ : -1]][ : -20].to_numpy()
     y = dframe[list(dframe.columns)[-1 : ]][ : -20].to_numpy().reshape(len(dframe['y']) - 20, )
     X_validate = dframe[list(dframe.columns)[ : -1]][-20 : ].to_numpy()
     y_validate = dframe[list(dframe.columns)[-1 : ]][-20 : ].to_numpy()
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.08, random_state = 16)
-    model = SVC()
-
+    model = MLPClassifier(hidden_layer_sizes = (128, 128,), max_iter = 1000, verbose = 0)
+    cv_gen = ShuffleSplit(n_splits = 10, test_size = 0.25, random_state = 0)
+    model_gs = GridSearchCV(model,
+                            {
+                                'activation': ['relu'],
+                                'solver': ['adam'],
+                                'batch_size': [128]
+                            },
+                            scoring = 'accuracy',
+                            n_jobs = -1,
+                            cv = cv_gen
+                            )
+    model_gs.fit(X, y)
+    print(model_gs.best_params_)
+    print("Accuracy score", model_gs.best_score_)
+    #print(X_validate.shape)
+    for i in range(X_validate.shape[0]):
+        prediction = model_gs.best_estimator_.predict(X_validate[i].reshape(1, X_validate.shape[1]))
+        print("Predicted:", prediction)
+        print("Real:", y_validate[i])
+        print("")
+    return 0
 
 # M A I N
 #df = GetData("task2.txt")
@@ -97,7 +181,7 @@ for name in ['x_3', 'x_6', 'x_7', 'x_8', 'x_9', 'x_10']:
 '''
 
 for name in list(df.columns)[ : -1]:
-    df[name] = pd.Series(np.array([1000000*val for val in df[name].to_numpy()]))
+    df[name] = pd.Series(np.array([10*val for val in df[name].to_numpy()]))
 
 #GetInfo(df)
 
@@ -106,4 +190,6 @@ for val in df['y']:
     class_balance[val] += 1
 print("Процент каждого класса в выборке:", [100*class_balance[key]/len(df['y']) for key in class_balance])
 
-KNNClassifier(df)
+#KNNClassifier(df)
+
+NeuralClassifier(df)
