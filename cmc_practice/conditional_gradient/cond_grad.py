7975292a68b6a4e2a434fc842030675a652c7170
nickdenisov777 <nick_denisov_777@mail.ru>
2020-09-16 23:30:21 +0300

    No RPN,  working on Hessian

--- cmc_practice/conditional_gradient/cond_grad.py
+++ cmc_practice/conditional_gradient/cond_grad.py
@@ -1,5 +1,6 @@
 import numpy as np
 
+'''
 class Functional():
     def __init__(self, input_str):
         input_str_tmp = input_str.replace('<', '(').replace('>', ')').replace(',', ')#(')
@@ -50,17 +51,65 @@ class Functional():
                 self.func_rpn.append(elem)
         while operands:
             self.func_rpn.append(operands.pop())
+'''
+
+
+class Functional():
+    def __init__(self, input_func, input_lims):
+        self.func = input_func
+        self.limits = input_lims
+        self.u_k = np.random.random(5) #vector of normally distributed values from [0.0, 1.0)
+
+    def Gradient(self, point):
+        grad = []
+        dim = len(point)
+        h = 0.0001 #обдумать этот моментик
+        for pos, num in enumerate(point):
+            dimension_f = np.array(point)
+            dimension_b = np.array(point)
+            dimension_f[pos] += h #шаг вперед
+            dimension_b[pos] -= h #шаг назад
+            print(dimension_f)
+            print(dimension_b)
+            print(self.func(dimension_f.tolist()))
+            print(self.func(dimension_b.tolist()))
+            grad.append((self.func(dimension_f.tolist()) - self.func(dimension_b.tolist())) / (2 * h))
+        return grad
+
+    def Hessian(self, point):
+        hess = []
+        dim = len(point)
+        h = 0.0001 #обдумать этот моментик
+        for pos1, num1 in enumerate(point):
+            partial_derivative = []
+            for pos2, num2 in enumerate(point):
+                dimension_f = np.array(point)
+                dimension_m = np.array(point)
+                dimension_b = np.array(point)
+                if pos1 == pos2:
+                    dimension_f[pos1] = num1 + h #шаг вперед
+                    dimension_m[pos1] = num1 #центральная точка
+                    dimension_b[pos1] = num1 - h #шаг назад
+                    partial_derivative.append((self.func(dimension_f.tolist()) + self.func(dimension_b.tolist()) - 2*self.func(dimension_m.tolist())) / (h**2))
+                else:
+                    dimension_f[pos1] += h #шаг вперед, координата 1
+                    dimension_b[pos1] -= h #шаг назад, координата 1
+                    dimension_f[pos2] += h #фиксируем координату 2
+                    dimension_b[pos2] += h #фиксируем координату 2
+                    der1 = (self.func(dimension_f.tolist()) - self.func(dimension_b.tolist())) / (2 * h) #производная по координате 1
+                    dimension_f[pos1] = num1
+                    dimension_b[pos1] = num1
+                    print(der1)
+                    dimension_f[pos2] -= h #шаг вперед, координата 2
+                    dimension_b[pos2] -= h #шаг назад, координата 2
+                    der2 = der1 - (self.func(dimension_f.tolist()) - self.func(dimension_b.tolist())) / (2 * h) #производная по координате 2
+                    partial_derivative.append(der2)
+            hess.append(partial_derivative)
+        return hess
 
 
 
 # M A I N
-u_initial = np.random.random(5) #vector of normally distributed values from [0.0, 1.0)
-#print(u_initial)
-actions = ['+', '-', '*', '/', '#', '<', '>', '(', ')'] #math operations
-
-print("Введите функционал для минизации, используя < и > для скалярного произведения и не пропуская знак умножения и без пробелов")
-
-funct = Functional(input())
-print(funct.func)
-funct.rpn()
-print(funct.func_rpn)
+funct = Functional(lambda x: x[0]**3 + (x[0]**2)*(x[1]**2) + x[1]**3, lambda y: 2 - y[0] + 1 - y[1] - y[0])
+print(funct.Gradient([2.0, 5.0]))
+print(funct.Hessian([2.0, 5.0]))
