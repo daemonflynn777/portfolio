d5aa25ea2f7b331693da68643784dec9247bc9bb
nickdenisov777 <nick_denisov_777@mail.ru>
2021-03-11 00:32:42 +0300

    TSP in progress...

--- cmc_practice/TSP_simulated_annealing/tsp.py
+++ cmc_practice/TSP_simulated_annealing/tsp.py
@@ -1,5 +1,7 @@
 import numpy as np
 import scipy as sp
+import math
+import matplotlib.pyplot as plt
 
 class TSP():
     def __init__(self, cities_amount = 50, cities_input = None):
@@ -14,7 +16,7 @@ class TSP():
             self.cities = np.array([cities_x, cities_y], dtype = np.float_)
         self.start_point = np.array(list(range(1, self.cities.shape[1] + 1)))
         np.random.shuffle(self.start_point)
-        self.T_max = 100
+        self.T_max = 1000
         self.T_min = 0.01
 
     def __objective_function(self, cities_order): #целевая функция суммарного расстояния между текущей последовательностью городов (закрытый метод класса)
@@ -30,17 +32,63 @@ class TSP():
         return self.T_max/iteration
 
     def __generate_next_point(self, prev_point): #функция генерации следующей последовательности городов (закрытый метод класса)
-        print(prev_point)
         positions = np.arange(prev_point.shape[0])
         np.random.shuffle(positions)
+        tmp_point = prev_point.copy()
         pos1 = positions[0]
         pos2 = positions[1]
-        prev_point[[pos1, pos2]] = prev_point[[pos2, pos1]]
-        print(prev_point)
+        #pos3 = positions[2]
+        #np.random.shuffle(tmp_point)
+        #tmp_point[[min(pos1, pos2) : max(pos1, pos2)]] = np.flip(tmp_point[min(pos1, pos2) : max(pos1, pos2)])
+        tmp_point1 = tmp_point[ : min(pos1, pos2)]
+        tmp_point2 = np.flip(tmp_point[min(pos1, pos2) : max(pos1, pos2)])
+        tmp_point3 = tmp_point[max(pos1, pos2) : ]
+        tmp_point = np.append(tmp_point1, tmp_point2)
+        tmp_point = np.append(tmp_point, tmp_point3)
+        #tmp_point = np.roll(tmp_point, 13)
+        #tmp_point = np.flip(tmp_point)
+        return tmp_point
 
-    def OptimizeRoute(self):
+    def __get_probability(self, curr_point, next_point, iteration): #вычисляем вероятность принятия следующей последовательности гоордов (закрытый метод класса)
+        if self.__objective_function(next_point) < self.__objective_function(curr_point):
+            return 1.0
+        else:
+            return math.exp(-(self.__objective_function(next_point) - self.__objective_function(curr_point))/self.__temperature_decrease(iteration))
+
+    def OptimizeRoute(self): #поиск оптимальной последовательности городов
+        iteration = 1
         initial_point = self.start_point
-
+        curr_point = self.start_point.copy()
+        next_point = self.__generate_next_point(curr_point)
+        probability = self.__get_probability(curr_point, next_point, iteration)
+        while self.__temperature_decrease(iteration) >= self.T_min:
+            print(probability)
+            if probability < 1.0:
+                ksi = abs(np.random.normal(0.0, 0.1, 1)[0])
+                if ksi > probability:
+                    print(iteration)
+                    #next_point = curr_point.copy()
+                    break
+            iteration += 1
+            curr_point = next_point.copy()
+            next_point = self.__generate_next_point(curr_point)
+            probability = self.__get_probability(curr_point, next_point, iteration)
+        self.end_point = curr_point.copy()
+        print("Конечная последовательность городов:", self.end_point)
+        print("Исходное суммарное расстояние:", self.__objective_function(initial_point))
+        print("Конечное суммарное расстояние:", self.__objective_function(self.end_point))
+        initial_matrix = np.array([[self.cities[0, i - 1], self.cities[1, i - 1]] for i in initial_point]).T
+        end_matrix = np.array([[self.cities[0, i - 1], self.cities[1, i - 1]] for i in self.end_point]).T
+        fig = plt.figure(figsize = (12, 4), num = "TSP")
+        plt.subplot(1, 3, 1)
+        plt.scatter(self.cities[0], self.cities[1])
+        plt.subplot(1, 3, 2)
+        plt.scatter(self.cities[0], self.cities[1])
+        plt.plot(initial_matrix[0], initial_matrix[1])
+        plt.subplot(1, 3, 3)
+        plt.scatter(self.cities[0], self.cities[1])
+        plt.plot(end_matrix[0], end_matrix[1])
+        #plt.show()
 
     def test(self):
         self.__generate_next_point(self.start_point)
@@ -49,5 +97,5 @@ class TSP():
 
 # M A I N
 cities_file = open("cities.txt", "r")
-tsp = TSP(10, cities_file)
-tsp.test()
+tsp = TSP(20)
+tsp.OptimizeRoute()
