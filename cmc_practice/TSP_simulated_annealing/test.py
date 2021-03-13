8f9673687f2e408856404ca716b25b124acb70e6
nickdenisov777 <nick_denisov_777@mail.ru>
2021-03-13 22:32:20 +0300

    TSP final release

---
+++ cmc_practice/TSP_simulated_annealing/test.py
@@ -0,0 +1,26 @@
+import tsp
+import numpy as np
+
+def TestTSP():
+    tsp_test = tsp.TSP()
+    tsp_test.OptimizeRoute()
+    print("Test 1 passed.\n")
+
+    cities_file = open("cities.txt", "r")
+    tsp_test = tsp.TSP(cities_file)
+    tsp_test.OptimizeRoute()
+    print("Test 2 passed.\n")
+    cities_file.close()
+
+    cities_list = [[1.5, 7.2, 5.2, 9.9], [2.28 , 6.66, 8.34, 4.04]]
+    tsp_test = tsp.TSP(cities_list)
+    tsp_test.OptimizeRoute()
+    print("Test 3 passed.\n")
+
+    cities_array = np.array([[2.28 , 6.66, 8.34, 4.04], [1.5, 7.2, 5.2, 9.9]])
+    tsp_test = tsp.TSP(cities_list)
+    tsp_test.OptimizeRoute()
+    print("Test 4 passed.\n")
+
+# M A I N
+TestTSP()
--- cmc_practice/TSP_simulated_annealing/tsp.py
+++ cmc_practice/TSP_simulated_annealing/tsp.py
@@ -1,23 +1,44 @@
 import numpy as np
-import scipy as sp
 import math
 import matplotlib.pyplot as plt
+import io
+import numbers
 
 class TSP():
-    def __init__(self, cities_amount = 50, cities_input = None):
-        if cities_input == None:
-            self.cities = np.random.uniform(0.0, 1.0, (2, cities_amount))*10 #cities[0] - координата X, cities[1] - координата Y
-            print(self.cities)
-        else:
+    def __init__(self, cities_input = 10):
+        if isinstance(cities_input, int):
+            if cities_input >= 2:
+                self.cities = np.random.uniform(0.0, 1.0, (2, cities_input))*10 #cities[0] - координата X, cities[1] - координата Y
+            else:
+                raise ValueError("Неправильное количество городов. Их должно быть больше двух.")
+        
+        elif isinstance(cities_input, list):
+            try:
+                self.cities = np.array(cities_input)
+            except:
+                raise ValueError("Проверьте данные. Списиок координат должен быть двумерным и содержать одинковое число (>= 2) координат X и Y.")
+        
+        elif isinstance(cities_input, np.ndarray):
+            if cities_input.shape[0] == 2 and cities_input.shape[1] >= 2:
+                self.cities = cities_input.copy()
+            else:
+                raise ValueError("Проверьте данные. Массив координат должен быть двумерным и содержать одинковое число (>= 2) координат X и Y.")
+        
+        elif isinstance(cities_input, io.IOBase):
             cities_x = list(map(float, cities_input.readline().split()))
             cities_y = list(map(float, cities_input.readline().split()))
             if len(cities_x) != len(cities_y):
                 raise ValueError(f"Количество координат по X и по Y должно совпадать.\nКоординат X: {len(cities_x)}, координат Y: {len(cities_y)}")
             self.cities = np.array([cities_x, cities_y], dtype = np.float_)
+        
+        else:
+            raise ValueError("Неверный формат входных данных. Возможные форматы: int, list, NumPy ndarray, file.")
+
         self.start_point = np.array(list(range(1, self.cities.shape[1] + 1)))
         np.random.shuffle(self.start_point)
         self.T_max = 1000
         self.T_min = 0.01
+        self.max_iter_no_change = self.cities.shape[1]*(self.cities.shape[1] - 1)/2
 
     def __objective_function(self, cities_order): #целевая функция суммарного расстояния между текущей последовательностью городов (закрытый метод класса)
         distance = 0.0
@@ -37,16 +58,6 @@ class TSP():
         tmp_point = prev_point.copy()
         pos1 = positions[0]
         pos2 = positions[1]
-        #pos3 = positions[2]
-        #np.random.shuffle(tmp_point)
-        #tmp_point[[min(pos1, pos2) : max(pos1, pos2)]] = np.flip(tmp_point[min(pos1, pos2) : max(pos1, pos2)])
-        #tmp_point1 = tmp_point[ : min(pos1, pos2)]
-        #tmp_point2 = np.flip(tmp_point[min(pos1, pos2) : max(pos1, pos2)])
-        #tmp_point3 = tmp_point[max(pos1, pos2) : ]
-        #tmp_point = np.append(tmp_point1, tmp_point2)
-        #tmp_point = np.append(tmp_point, tmp_point3)
-        #tmp_point = np.roll(tmp_point, 13)
-        #tmp_point = np.flip(tmp_point)
         tmp_point[[pos1, pos2]] = tmp_point[[pos2, pos1]]
         return tmp_point
 
@@ -59,25 +70,20 @@ class TSP():
     def OptimizeRoute(self): #поиск оптимальной последовательности городов
         iteration = 1
         initial_point = self.start_point
-        #curr_point = self.start_point.copy()
-        #next_point = self.__generate_next_point(curr_point)
         next_point = self.start_point.copy()
-        #probability = self.__get_probability(curr_point, next_point, iteration)
+        iter_no_change = 0
         while self.__temperature_decrease(iteration) >= self.T_min:
             curr_point = next_point.copy()
             next_point = self.__generate_next_point(curr_point)
             probability = self.__get_probability(curr_point, next_point, iteration)
-            print(probability)
             if probability < 1.0:
                 ksi = abs(np.random.uniform(-1.0, 1.0, 1)[0])
                 if ksi >= probability:
-                    print(iteration)
                     next_point = curr_point.copy()
-                    #break
+                    iter_no_change += 1
+            if (curr_point != next_point).all():
+                iter_no_change = 0
             iteration += 1
-            #curr_point = next_point.copy()
-            #next_point = self.__generate_next_point(curr_point)
-            #probability = self.__get_probability(curr_point, next_point, iteration)
         self.end_point = next_point.copy()
         print("Конечная последовательность городов:", self.end_point)
         print("Исходное суммарное расстояние:", self.__objective_function(initial_point))
@@ -85,22 +91,12 @@ class TSP():
         initial_matrix = np.array([[self.cities[0, i - 1], self.cities[1, i - 1]] for i in initial_point]).T
         end_matrix = np.array([[self.cities[0, i - 1], self.cities[1, i - 1]] for i in self.end_point]).T
         fig = plt.figure(figsize = (12, 4), num = "TSP")
-        plt.subplot(1, 3, 1)
+        plt.subplot(1, 3, 1, title = "Города")
         plt.scatter(self.cities[0], self.cities[1])
-        plt.subplot(1, 3, 2)
+        plt.subplot(1, 3, 2, title = "Начальный путь")
         plt.scatter(self.cities[0], self.cities[1])
         plt.plot(initial_matrix[0], initial_matrix[1])
-        plt.subplot(1, 3, 3)
+        plt.subplot(1, 3, 3, title = "Оптимальный путь")
         plt.scatter(self.cities[0], self.cities[1])
         plt.plot(end_matrix[0], end_matrix[1])
         plt.show()
-
-    def test(self):
-        self.__generate_next_point(self.start_point)
-        
-
-
-# M A I N
-cities_file = open("cities.txt", "r")
-tsp = TSP(20)
-tsp.OptimizeRoute()
